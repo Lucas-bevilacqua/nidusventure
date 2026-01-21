165956d0d0d0cab82efb02edddbdd973995ea268
gitmkt123 <lucas.bevilacqua@idealtrends.com.br>
2026-01-21 11:14:48 -0300

    Corrige erro de TypeScript: adiciona importaÃ§Ã£o do React no MarkdownContent

--- components/blog/MarkdownContent.tsx
+++ components/blog/MarkdownContent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo } from "react";
+import React, { useMemo } from "react";
 import Link from "next/link";
 
 export function MarkdownContent({ content }: { content: string }) {
